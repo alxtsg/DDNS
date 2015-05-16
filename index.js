7a56276df4f3413c7a5127ccec45355e6a8143c7
Alex Tsang <alextsang@live.com>
2015-05-16 15:36:04 +0800

    Initial commit.

---
+++ index.js
@@ -0,0 +1,108 @@
+/*
+  Main program.
+
+  @author alextsang@live.com
+*/
+(function(){
+
+  'use strict';
+
+  var fs = require('fs'),
+    https = require('https'),
+    queryString = require('querystring'),
+    StringDecoder = require('string_decoder').StringDecoder,
+    util = require('util'),
+
+    decoder = new StringDecoder('utf8'),
+
+    // Interval between updating DDNS record, in milliseconds.
+    updateInterval = 0,
+
+    // Path to log file.
+    logFilePath = null,
+
+    // Query parameter string to be sent to Namecheap.
+    queryParamString = null,
+
+    // HTTPS request options.
+    requestOptions = null,
+
+    /*
+      Parses configuration file.
+
+      @param data Configuration data.
+    */
+    parseConfig = function(data){
+      try{
+        var config = JSON.parse(data);
+        queryParamString = queryString.stringify({
+          host: config.host,
+          domain: config.domain,
+          password: config.password
+        });
+        logFilePath = config.log;
+        // Convert update interval from seconds to milliseconds.
+        updateInterval = config.updateInterval * 1000;
+        requestOptions = {
+          hostname: 'dynamicdns.park-your-domain.com',
+          port: 443,
+          path: '/update?' + queryParamString,
+          method: 'GET'
+        };
+      }catch(error){
+        console.error(
+          util.format('Unable to parse configuratiln file: %s', error));
+        process.exit(1);
+      }
+    },
+
+    /*
+      Writes message to log file.
+
+      @param message Log message.
+    */
+    writeLog = function(message){
+      message += '\n';
+      fs.appendFile(logFilePath, message, function(error){
+        if(error !== null){
+          console.error('Unable to write log.');
+        }
+      });
+    },
+
+    /*
+      Updates DDNS record.
+    */
+    updateRecord = function(){
+      var request = https.request(requestOptions, function(response){
+            // Actual status is included in response body as an XML document.
+            writeLog(util.format('Status code: %d', response.statusCode));
+            response.on('data', function(data){
+              writeLog(decoder.write(data));
+            });
+          });
+      request.end();
+      request.on('error', function(error){
+        writeLog(util.format('Error: %s', error));
+      });
+    },
+
+    /*
+      Updates DDNS record periodically.
+    */
+    startPeriodicUpdate = function(){
+      setInterval(updateRecord, updateInterval);
+    };
+
+  // Read configuration file and start updating DDNS record periodically.
+  fs.readFile('config.json', {
+    encoding: 'utf8'
+  }, function(error, data){
+    if(error !== null){
+      console.error('Unable to read configuration file.');
+      process.exit(1);
+    }
+    parseConfig(data);
+    startPeriodicUpdate();
+  });
+}());
